99e530c045c68250cdf50016fa21e5ec8c7ad3f7
Derya Kalender <derya.sdev@gmail.com>
2025-07-09 12:05:17 +0300

    Lesson 19 - Table Status Momentary added.

--- MyUdemy20Projects/Project6_ApiWeather/obj/Debug/net6.0/Project6_ApiWeather.AssemblyInfo.cs
+++ MyUdemy20Projects/Project6_ApiWeather/obj/Debug/net6.0/Project6_ApiWeather.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project6_ApiWeather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9605264de3524599b9860412fabc0d4bb8e1103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7eb72caf8355721de0747869e18738af8a0246")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project6_ApiWeather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project6_ApiWeather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyUdemy20Projects/Project7_ApiWeatherConsume/obj/Debug/net6.0/Project7_ApiWeatherConsume.AssemblyInfo.cs
+++ MyUdemy20Projects/Project7_ApiWeatherConsume/obj/Debug/net6.0/Project7_ApiWeatherConsume.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project7_ApiWeatherConsume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9605264de3524599b9860412fabc0d4bb8e1103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7eb72caf8355721de0747869e18738af8a0246")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project7_ApiWeatherConsume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project7_ApiWeatherConsume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
